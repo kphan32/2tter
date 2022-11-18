503fba4e05df3053c4a5bd3bacecc84e1920d1e4
Kavin Phan <kavin@kphan.tech>
2022-11-17 23:17:35 -0500

    Create basic layout

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import { Layout } from "../components/Layout";
+
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>2tter</h1>
+    </Layout>
+  );
+};
+
+export default Home;
